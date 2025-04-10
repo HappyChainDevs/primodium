f21bdac16882de72f36fe43b3a3211c298b8af35
ultraviolet10 <aritrac1998@hotmail.com>
2025-04-10 19:01:07 +0530

    refactor: Enter page session key flow

--- packages/client/src/util/localStorage.ts
+++ packages/client/src/util/localStorage.ts
@@ -2,6 +2,8 @@ import { Hex } from "viem";
 
 import { STORAGE_PREFIX } from "@primodiumxyz/core";
 
+export const HAPPY_STORAGE_PREFIX = "happySessionRegistered:";
+
 export function findEntriesWithPrefix(prefix: string = STORAGE_PREFIX) {
   // Array to hold the matched entries
   const matchedEntries = [];
