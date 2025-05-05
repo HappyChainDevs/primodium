9567142d3d15064f0c21fbf228c9bc5cf5f4dc09
ultraviolet10 <aritrac1998@hotmail.com>
2025-05-05 12:39:13 +0530

    connect w happychain and nothing else

--- packages/client/src/config/wagmiConfig.ts
+++ packages/client/src/config/wagmiConfig.ts
@@ -1,4 +1,5 @@
 import { createHappyChainWagmiConfig, happyChainSepolia } from "@happy.tech/core";
 import { Config } from "wagmi";
 
+// [HAPPY_PRIM] use custom config tailored to HappyChain
 export const wagmiConfig: Config = createHappyChainWagmiConfig(happyChainSepolia);
